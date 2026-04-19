de2e4b7020153771cd8c379c2919403a68a06084
Dibash <thapadibash22@gmail.com>
2026-04-19 21:51:56 +0545

    Trying different function of vim and lazygit

--- init.lua
+++ init.lua
@@ -8,29 +8,36 @@ vim.o.relativenumber = true -- Relative line numbers (easier jumping)
 vim.o.mouse = 'a'           -- Enable mouse support
 vim.o.ignorecase = true     -- Ignore case in search
 vim.o.smartcase = true      -- ...unless search has capital letters
-vim.o.shiftwidth = 4        -- Size of an indent
+vim.o.shiftwidth = 2        -- Size of an indent
 vim.o.expandtab = true      -- Use spaces instead of tabs
 vim.o.termguicolors = true  -- Better colors
-
---Sync clipboards
-vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+vim.o.scrolloff = 10        -- Keep 10 line below/above cursor line
+vim.o.sidescrolloff = 10    -- Keep 10 line left/right cusrsor line
 
 --Raise dialog in unsaved buffer
 vim.o.confirm = true
 
 --Snappy escape
-vim.o.timeoutlen = 400
+vim.o.updatetime = 250
+vim.o.timeoutlen = 250
+
+--How new window appear
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+--Sync clipboards
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
 --Vim diagnostic
 vim.diagnostic.config({
-    severity_sort = true,
-    update_in_insert = false,
-    float = { source = 'if_many' },
-    jump = { float = true },
+  severity_sort = true,
+  update_in_insert = false,
+  float = { source = 'if_many' },
+  jump = { float = true },
 })
 
 --Show diagnostics
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostic' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.open_float, { desc = 'Show diagnostic' })
 
 --Easily move beteween windows
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
@@ -40,48 +47,58 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 --Highlights yanks
 vim.api.nvim_create_autocmd('TextYankPost', {
-    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-    callback = function() vim.highlight.on_yank() end
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function() vim.highlight.on_yank() end
 })
 
 --Plugins
 vim.pack.add({
-    'https://github.com/ibhagwan/fzf-lua',
-    'https://github.com/nvim-treesitter/nvim-treesitter',
-    'https://github.com/neovim/nvim-lspconfig',
-    'https://github.com/stevearc/oil.nvim',
-    'https://github.com/kdheepak/lazygit.nvim',
-    'https://github.com/esmuellert/codediff.nvim',
-    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
-    'https://github.com/goolord/alpha-nvim',
-    'https://github.com/rebelot/kanagawa.nvim',
-    { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('1.x') }, -- pinning so rust binary dependency automatically downloads
+  'https://github.com/ibhagwan/fzf-lua',
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/stevearc/oil.nvim',
+  'https://github.com/kdheepak/lazygit.nvim',
+  'https://github.com/esmuellert/codediff.nvim',
+  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+  'https://github.com/goolord/alpha-nvim',
+  'https://github.com/rebelot/kanagawa.nvim',
+  { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('1.x') }, -- pinning so rust binary dependency automatically downloads
 
 })
 
 --Fzf-lua
 require("fzf-lua").setup({
-    keymap = {
-        builtin = {
-            ["<C-d>"] = 'preview-page-down',
-            ["<C-u>"] = 'preview-page-up',
-        },
+  keymap = {
+    builtin = {
+      ["<C-d>"] = 'preview-page-down',
+      ["<C-u>"] = 'preview-page-up',
     },
+  },
+  winopts = {
+    backdrop = 60,
+  },
+  --fzf_colors = true,
 })
 
 --Kanagawa
 require('kanagawa').setup({
-    colors = {
-        theme = {
-            all = {
-                ui = {
-                    bg_gutter = "none"
-                }
-            }
+  colors = {
+    theme = {
+      all = {
+        ui = {
+          bg_gutter = "none"
         }
+      }
     }
+  }
 })
 vim.cmd('colorscheme kanagawa-wave')
+
+-- Configure cursor style and appearance
+-- vim.o.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50'
+
+-- Style the cursor with underline
+-- vim.api.nvim_set_hl(0, 'Cursor', { underline = true, cterm = { underline = true } })
 
 --Markdown
 require('render-markdown').setup({})
@@ -89,39 +106,37 @@ require('render-markdown').setup({})
 vim.keymap.set('n', '<leader><leader>', '<cmd>FzfLua files<cr>', { desc = 'Find files' })
 vim.keymap.set('n', '<leader>/', '<cmd>FzfLua live_grep<cr>', { desc = 'Find live grep' })
 
-
---Tree_stter
+--Tree_sitter
 vim.cmd('syntax off')
 vim.api.nvim_create_autocmd('FileType', {
-    callback = function() pcall(vim.treesitter.start) end,
+  callback = function() pcall(vim.treesitter.start) end,
 })
-
 
 --LSP
 vim.lsp.enable({
-    'ty',
-    'ruff',
-    'lua_ls',
-    'ts_ls',
+  'ty',
+  'ruff',
+  'lua_ls',
+  'ts_ls',
 })
 vim.o.signcolumn = 'yes'
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
 -- Auto-format ("lint") on save (adapted from neovim docs :help auto-format)
 vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('my.lsp', { clear = true }),
-    callback = function(ev)
-        local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-        if not client:supports_method('textDocument/willSaveWaitUntil')
-            and client:supports_method('textDocument/formatting') then
-            vim.api.nvim_create_autocmd('BufWritePre', {
-                group = vim.api.nvim_create_augroup('my.lsp.fmt', { clear = false }),
-                buffer = ev.buf,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
-                end,
-            })
-        end
-    end,
+  group = vim.api.nvim_create_augroup('my.lsp', { clear = true }),
+  callback = function(ev)
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+    if not client:supports_method('textDocument/willSaveWaitUntil')
+        and client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('my.lsp.fmt', { clear = false }),
+        buffer = ev.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+  end,
 })
 
 
@@ -130,9 +145,9 @@ require('blink.cmp').setup({})
 
 --Oil
 require("oil").setup({
-    view_options = {
-        show_hidden = true,
-    },
+  view_options = {
+    show_hidden = true,
+  },
 })
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
---
+++ init_lua.md
@@ -0,0 +1,184 @@
+
+--Set Leader
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Basic Settings
+vim.o.number = true         -- Show line numbers
+vim.o.relativenumber = true -- Relative line numbers (easier jumping)
+vim.o.mouse = 'a'           -- Enable mouse support
+vim.o.ignorecase = true     -- Ignore case in search
+vim.o.smartcase = true      -- ...unless search has capital letters
+vim.o.shiftwidth = 4        -- Size of an indent
+vim.o.expandtab = true      -- Use spaces instead of tabs
+vim.o.termguicolors = true  -- Better colors
+
+--Sync clipboards
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+
+--Raise dialog in unsaved buffer
+vim.o.confirm = true
+
+--Snappy escape
+vim.o.timeoutlen = 400
+
+--Vim diagnostic
+vim.diagnostic.config({
+    severity_sort = true,
+    update_in_insert = false,
+    float = { source = 'if_many' },
+    jump = { float = true },
+})
+
+--Show diagnostics
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostic' })
+
+--Easily move beteween windows
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+--Highlights yanks
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    callback = function() vim.highlight.on_yank() end
+})
+
+--Plugins
+vim.pack.add({
+    'https://github.com/ibhagwan/fzf-lua',
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/neovim/nvim-lspconfig',
+    'https://github.com/stevearc/oil.nvim',
+    'https://github.com/kdheepak/lazygit.nvim',
+    'https://github.com/esmuellert/codediff.nvim',
+    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+    'https://github.com/goolord/alpha-nvim',
+    'https://github.com/rebelot/kanagawa.nvim',
+    { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('1.x') }, -- pinning so rust binary dependency automatically downloads
+
+})
+
+--Fzf-lua
+require("fzf-lua").setup({
+    keymap = {
+        builtin = {
+            ["<C-d>"] = 'preview-page-down',
+            ["<C-u>"] = 'preview-page-up',
+        },
+    },
+    -- winopts = {
+    --backdrop = 60,
+
+    --},
+    winopts = {
+        height = 0.85,      -- 0-1 relative to screen
+        width = 0.80,       -- 0-1 relative to screen
+        row = 0.5,          -- vertical position (0 top, 1 bottom)
+        col = 0.5,          -- horizontal position (0 left, 1 right)
+
+        border = "rounded", -- none, single, double, rounded, shadow, or custom
+        backdrop = 60,      -- dim background (0-100)
+
+        fullscreen = false
+    }, -- override to full screen
+
+    preview = {
+        border = "border",     -- preview window border
+        wrap = false,
+        hidden = "hidden",     -- hide preview toggle
+        vertical = "down:45%", -- layout control
+    },
+
+    hl = {
+        normal = "Normal",
+        border = "FloatBorder",
+        preview_normal = "Normal",
+        preview_border = "FloatBorder",
+    },
+    --fzf_colors = true,
+})
+
+--Kanagawa
+require('kanagawa').setup({
+    colors = {
+        theme = {
+            all = {
+                ui = {
+                    bg_gutter = "none"
+                }
+            }
+        }
+    }
+})
+vim.cmd('colorscheme kanagawa-wave')
+
+-- Configure cursor style and appearance
+-- vim.o.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50'
+
+-- Style the cursor with underline
+-- vim.api.nvim_set_hl(0, 'Cursor', { underline = true, cterm = { underline = true } })
+
+--Markdown
+require('render-markdown').setup({})
+
+vim.keymap.set('n', '<leader><leader>', '<cmd>FzfLua files<cr>', { desc = 'Find files' })
+vim.keymap.set('n', '<leader>/', '<cmd>FzfLua live_grep<cr>', { desc = 'Find live grep' })
+
+
+--Tree_stter
+vim.cmd('syntax off')
+vim.api.nvim_create_autocmd('FileType', {
+    callback = function() pcall(vim.treesitter.start) end,
+})
+
+
+--LSP
+vim.lsp.enable({
+    'ty',
+    'ruff',
+    'lua_ls',
+    'ts_ls',
+})
+vim.o.signcolumn = 'yes'
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
+-- Auto-format ("lint") on save (adapted from neovim docs :help auto-format)
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('my.lsp', { clear = true }),
+    callback = function(ev)
+        local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+        if not client:supports_method('textDocument/willSaveWaitUntil')
+            and client:supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                group = vim.api.nvim_create_augroup('my.lsp.fmt', { clear = false }),
+                buffer = ev.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+                end,
+            })
+        end
+    end,
+})
+
+
+--Blink
+require('blink.cmp').setup({})
+
+--Oil
+require("oil").setup({
+    view_options = {
+        show_hidden = true,
+    },
+})
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+--Lazygit
+vim.keymap.set('n', '<leader>g', '<cmd>LazyGit<cr>', { desc = 'LazyGit' })
+
+-- Codediff (vscode like diffs :))
+require("codediff").setup({})
+
+--DAP
+
+--Neovim Homescreen Configuration
