e021a5d02fce49b6be62114603a728a9cca97881
Dibash <thapadibash22@gmail.com>
2026-05-14 12:25:14 +0545

    Removing tmux navigator and smear-cursor effect

--- nvim/init.lua
+++ nvim/init.lua
@@ -241,38 +241,6 @@ require("lazy").setup({
       },
     },
   },
-  -- Cursor animations
-  -- {
-  --   "sphamba/smear-cursor.nvim",
-  --   opts = {
-  --     smear_between_buffers = true,
-  --     smear_between_neighbor_lines = true,
-  --     scroll_buffer_space = true,
-  --     legacy_computing_symbols_support = true,
-  --     stiffness = 0.5,
-  --     trailing_stiffness = 0.3,
-  --     distance_stop_animating = 0.1,
-  --     smear_to_cmd = true,
-  --     hide_target_hack = false,
-  --   },
-  -- },
-  -- {
-  --   "christoomey/vim-tmux-navigator",
-  --   cmd = {
-  --     "TmuxNavigateLeft",
-  --     "TmuxNavigateDown",
-  --     "TmuxNavigateUp",
-  --     "TmuxNavigateRight",
-  --     "TmuxNavigatePrevious",
-  --   },
-  --   keys = {
-  --     { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-  --     { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-  --     { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-  --     { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-  --     { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-  --   },
-  -- },
   -- Mini plugins
   { "echasnovski/mini.ai",                           version = "*", opts = {} },
   { "echasnovski/mini.comment",                      version = "*", opts = {} },
