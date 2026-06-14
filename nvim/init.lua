107f763690c7c788c6f07b1cdf5782f193cee371
DibashT <thapadibash22@gmail.com>
2026-06-14 20:48:13 +0545

    Debugpy changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -209,7 +209,7 @@ pcall(function()
   require("nvim-treesitter").install(parsers)
 end)
 
---- Statusline (Lualine)
+-- Statusline (Lualine)
 require("lualine").setup({
   options = {
     theme = "kanagawa",
@@ -218,10 +218,10 @@ require("lualine").setup({
   },
 })
 
---Markdown
+-- Markdown
 require("render-markdown").setup({})
 
---- Mini Plugins
+-- Mini Plugins
 require("mini.ai").setup({})
 require("mini.comment").setup({})
 require("mini.move").setup({})
@@ -328,6 +328,38 @@ require('blink.cmp').setup({
 })
 
 -- Dap (debugging)
+-- local dap = require('dap')
+-- dap.adapters.debugpy = function(cb, config) -- also $ uv tool install debugpy@latest
+--   if config.request == 'attach' then
+--     cb({
+--       type = 'server',
+--       port = config.connect.port,
+--       host = config.connect.host or '127.0.0.1',
+--     })
+--   else
+--     cb({
+--       type = 'executable',
+--       command = 'debugpy-adapter',
+--     })
+--   end
+-- end
+-- dap.configurations.python = { -- https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
+--   {
+--     type = 'debugpy',
+--     request = 'launch',
+--     name = 'Launch file',
+--     program = '${file}',
+--     python = function()
+--       local root = vim.fs.root(0, '.venv')
+--       return { root and root .. '/.venv/bin/python' or 'python3' }
+--     end,
+--     cwd = function()
+--       return vim.fs.root(0, '.venv') or vim.fn.getcwd()
+--     end,
+--   },
+-- }
+
+-- Dap (debugging)
 local dap = require('dap')
 dap.adapters.debugpy = function(cb, config) -- also $ uv tool install debugpy@latest
   if config.request == 'attach' then
@@ -349,6 +381,40 @@ dap.configurations.python = { -- https://github.com/microsoft/debugpy/wiki/Debug
     request = 'launch',
     name = 'Launch file',
     program = '${file}',
+    justMyCode = false,
+    python = function()
+      local root = vim.fs.root(0, '.venv')
+      return { root and root .. '/.venv/bin/python' or 'python3' }
+    end,
+    cwd = function()
+      return vim.fs.root(0, '.venv') or vim.fn.getcwd()
+    end,
+  },
+  {
+    type = 'debugpy',
+    request = 'launch',
+    name = 'Pytest current file',
+    module = 'pytest',
+    args = { '${file}', '-s' },
+    justMyCode = false,
+    python = function()
+      local root = vim.fs.root(0, '.venv')
+      return { root and root .. '/.venv/bin/python' or 'python3' }
+    end,
+    cwd = function()
+      return vim.fs.root(0, '.venv') or vim.fn.getcwd()
+    end,
+  },
+  {
+    type = 'debugpy',
+    request = 'launch',
+    name = 'Pytest current file -k',
+    module = 'pytest',
+    args = function()
+      local test_name = vim.fn.input('pytest -k: ')
+      return { '${file}', '-s', '-k', test_name }
+    end,
+    justMyCode = false,
     python = function()
       local root = vim.fs.root(0, '.venv')
       return { root and root .. '/.venv/bin/python' or 'python3' }
